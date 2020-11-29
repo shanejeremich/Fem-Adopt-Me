67fea5534c85b57cd88952648a0ef786142ff430
Shane Jeremich <shane.jeremich@gmail.com>
2020-11-29 06:40:51 -0700

    added settings to eslint

--- src/App.js
+++ src/App.js
@@ -3,24 +3,32 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Leo",
-      animal: "Cat",
-      breed: "Long Hair",
-    }),
-    React.createElement(Pet, {
-      name: "Calyx",
-      animal: "Cat",
-      breed: "Short Hair",
-    }),
-    React.createElement(Pet, {
-      name: "Tya",
-      animal: "Dog",
-      breed: "Labrador Retriever",
-    }),
-  ]);
+  // return React.createElement("div", {}, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Leo",
+  //     animal: "Cat",
+  //     breed: "Long Hair",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Calyx",
+  //     animal: "Cat",
+  //     breed: "Short Hair",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Tya",
+  //     animal: "Dog",
+  //     breed: "Labrador Retriever",
+  //   }),
+  // ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Leo" animal="Cat" breed="Long Hair" />
+      <Pet name="Calyx" animal="Cat" breed="Short air" />
+      <Pet name="Tya" animal="Dog" breed="Labrador Retriever" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));

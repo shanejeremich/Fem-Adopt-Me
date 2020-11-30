c4b1cc0fc4fa3e63229b2a97273796ac873c3741
Shane Jeremich <shane.jeremich@gmail.com>
2020-11-30 06:11:13 -0700

    async calls to Petfinder API, update Pet component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
-  // return React.createElement("div", {}, [
-  //   React.createElement("h1", {}, "Adopt Me!"),
-  //   React.createElement(Pet, {
-  //     name: "Leo",
-  //     animal: "Cat",
-  //     breed: "Long Hair",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Calyx",
-  //     animal: "Cat",
-  //     breed: "Short Hair",
-  //   }),
-  //   React.createElement(Pet, {
-  //     name: "Tya",
-  //     animal: "Dog",
-  //     breed: "Labrador Retriever",
-  //   }),
-  // ]);
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <Pet name="Leo" animal="Cat" breed="Long Hair" />
-      <Pet name="Calyx" animal="Cat" breed="Short air" />
-      <Pet name="Tya" animal="Dog" breed="Labrador Retriever" />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
